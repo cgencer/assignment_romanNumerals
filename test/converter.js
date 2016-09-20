bba79deee4e64460ed58a2fe275faed9d1c76cc1
Cem Gencer <o.cem.gencer@gmail.com>
2016-09-20 13:14:50 +0300

    start

--- test/converter.js
+++ test/converter.js
@@ -1,6 +1,10 @@
 var expect    = require("chai").expect;
 var converter = require("../app/converter");
 
+var testValues = [null, ‘’, 0, 1, 3, 4, 5, ‘I’, ‘III’, ‘IIII’, ‘IV’, ‘V’, 
+					1968, ‘1473’, 2999, 3000, 10000, ‘CDXXIX’, ‘CD1X’, 
+					‘error’, ‘MCDLXXXII’, ‘MCMLXXX’, ‘MMMMCMXCIX’, ‘MMMMDMXCIX’];
+
 describe("Roman Numerals converter", function() {
   describe("Roman to Latin conversion", function() {
     it("converts the numerals", function() {
