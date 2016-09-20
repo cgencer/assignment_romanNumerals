d3a1fcea516fa2c43923dcf9683eaa1f0432dfe3
Cem Gencer <o.cem.gencer@gmail.com>
2016-09-20 11:18:30 +0300

    kickstart

---
+++ test/converter.js
@@ -0,0 +1,16 @@
+var expect    = require("chai").expect;
+var converter = require("../app/converter");
+
+describe("Roman Numerals converter", function() {
+  describe("Roman to Latin conversion", function() {
+    it("converts the numerals", function() {
+
+    });
+  });
+
+  describe("Latin to Roman conversion", function() {
+    it("converts the number", function() {
+
+    });
+  });
+});
